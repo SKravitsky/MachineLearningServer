ef5c117f1e38dabdd257014c7b24fc8ccd0105a3
SKravitsky <drago509_6@hotmail.com>
2016-10-26 21:32:08 -0400

    Created a dictionary that holds the station names and the URL for the station itself

--- SeptaStation/StationPull.py
+++ SeptaStation/StationPull.py
@@ -6,17 +6,37 @@ import urllib2
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.septa.org/maps/transit/mfl.html'
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page, 'html5lib')
-soup.prettify()
+def ParseLinks(url, website):
+    try:
+        page = None
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, 'html5lib')
+    finally:
+        if page:
+            page.close()
+    StationDict = SoupLinks(soup)
+    return StationDict
 
-front = 'www.septa.org'
-for anchor in soup.findAll('area', href=True):
-    x = anchor['href']
-    print x
-    y = x[11:]
-    print y
-    z = front + y
-    print z
-    
+def SoupLinks(soup):
+    soup.prettify()
+    StationDict = {}
+    for anchor in soup.findAll('area', href=True):
+        x = anchor['href']
+        y = x[11:]
+        temp = y[25:]
+        StationName = temp[:-5]
+        if y[1:2] == 't':
+            pass
+        else:
+            FullURL = front + y
+        StationDict[FullURL] = StationName
+        print FullURL
+        print StationName
+    print StationDict
+    return StationDict
+
+if __name__ == "__main__":
+    url = 'http://www.septa.org/maps/transit/mfl.html'
+    front = 'www.septa.org'
+    StationDict = ParseLinks(url,front)
+    print StationDict
