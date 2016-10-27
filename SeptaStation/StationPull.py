b0c63764957c945b386fb770e3ac97bfed2edab5
SKravitsky <drago509_6@hotmail.com>
2016-10-27 00:27:11 -0400

    Need to test if the script is in an infite loop, if it isn't then it should be pulling all station lat long and storing it in a dictionary with format {'StationName,[Lat Long]',...}

--- SeptaStation/StationPull.py
+++ SeptaStation/StationPull.py
@@ -2,6 +2,7 @@
 #It will first pull the station names and URLs and put them in a dictionary
 #Using the URLs the Lat/Long can be scraped
 
+import re
 import urllib2
 import requests
 from bs4 import BeautifulSoup
@@ -21,22 +22,56 @@ def SoupLinks(soup):
     soup.prettify()
     StationDict = {}
     for anchor in soup.findAll('area', href=True):
-        x = anchor['href']
-        y = x[11:]
-        temp = y[25:]
-        StationName = temp[:-5]
-        if y[1:2] == 't':
+        link = anchor['href']
+        fixedlink = link[11:]
+        if fixedlink[1:2] == 't':
             pass
         else:
-            FullURL = front + y
-        StationDict[FullURL] = StationName
-        print FullURL
-        print StationName
-    print StationDict
+            StationName = fixedlink[25:-5]
+            FullURL = front + fixedlink
+            StationDict[FullURL] = StationName
     return StationDict
+
+def ParseLocation(StationDict):
+    LocationDict = {}
+    for key, value in StationDict.iteritems():
+        LatLong = ParseStations(key, value)
+        LocationDict[value] = LatLong
+    return LocationDict
+
+def ParseStations(url, station):
+    try:
+        page = None
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page, 'html5lib')
+    finally:
+        if page:
+            page.close()
+
+    LatLong = SoupStations(soup)
+    return LatLong
+
+def SoupStations(soup):
+    soup.prettify()    
+    header = str(soup.head)
+    paragraph = header[2000:]
+    
+    regex = re.search('(?<=LatLng)(.*)',paragraph)
+    
+    line = regex.group(1)
+    longlist = line.split('LatLng')
+    notlatlng = longlist[1]
+    latlnglist = notlatlng[1:-2]
+    LatLong = latlnglist.split(',')
+
+    return LatLong
+
+        
 
 if __name__ == "__main__":
     url = 'http://www.septa.org/maps/transit/mfl.html'
-    front = 'www.septa.org'
+    front = 'http://www.septa.org'
     StationDict = ParseLinks(url,front)
-    print StationDict
+    LocationDict = ParseLocation(StationDict)
+    print LocationDict
+##    print StationDict        
