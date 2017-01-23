82cd9b866de8f7fa5c6e85933a07112867894c57
Steven Kravitsky <SKravitsky@users.noreply.github.com>
2017-01-23 11:50:50 -0500

    Added Septa Service Advisory API

---
+++ api_test.py
@@ -0,0 +1,33 @@
+from urllib2 import Request, urlopen, URLError
+from pprint import pprint
+import json, re, string
+
+
+def cleanhtml(raw_html):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
+
+
+def request_status(request):
+	try:
+		response = urlopen(request)
+		test = response.read()
+	except URLError, e:
+		print "Didn't work, got error:",e
+	
+	return test
+
+
+if __name__ == "__main__":
+	request = Request('http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=rr_route_mfl')
+	output = request_status(request)
+	
+	out = json.loads(output)
+	node = out[0]['advisory_message']
+
+	x = cleanhtml(node)
+	y = re.sub(r"\t+", "", x)
+	print y
+
+
