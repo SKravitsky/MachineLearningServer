35e21003b0d34ad5f5e460b999be70ab531001df
Steven Kravitsky <SKravitsky@users.noreply.github.com>
2017-01-23 13:09:34 -0500

    Cleaning some formatting

--- api_test.py
+++ api_test.py
@@ -1,13 +1,13 @@
+import json
+import re
+import string
 from urllib2 import Request, urlopen, URLError
 from pprint import pprint
-import json, re, string
-
 
 def cleanhtml(raw_html):
 	cleanr = re.compile('<.*?>')
 	cleantext = re.sub(cleanr, '', raw_html)
 	return cleantext
-
 
 def request_status(request):
 	try:
@@ -15,9 +15,8 @@ def request_status(request):
 		test = response.read()
 	except URLError, e:
 		print "Didn't work, got error:",e
-	
-	return test
 
+	return test
 
 if __name__ == "__main__":
 	request = Request('http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=rr_route_mfl')
@@ -29,5 +28,3 @@ if __name__ == "__main__":
 	x = cleanhtml(node)
 	y = re.sub(r"\t+", "", x)
 	print y
-
-
