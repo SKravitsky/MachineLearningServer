4326f9162875a83c718d087f3e1a4edbc6c8383c
SKravitsky <drago509_6@hotmail.com>
2017-01-23 17:07:07 -0500

    Changed everything to be method based

--- api_test.py
+++ api_test.py
@@ -5,30 +5,40 @@ from urllib2 import Request, urlopen, URLError
 from pprint import pprint
 
 def clean_html(raw_html):
-	cleanr = re.compile('<.*?>')
-	cleantext = re.sub(cleanr, '', raw_html)
-	return cleantext
+    cleanr = re.compile('<.*?>')
+    clean_text = re.sub(cleanr, '', raw_html)
+    tabless_text = re.sub(r"\t+", "", clean_text)    
+    return tabless_text
+
+def json_formatting(data):
+    output = json.loads(data)
+    node = output[0]['advisory_message']
+    return node
 
 def request_status(request):
-	try:
-		response = urlopen(request)
-		test = response.read()
-	except URLError, e:
-		print "Didn't work, got error:",e
+    try:
+        response = urlopen(request)
+        test = response.read()
+    except URLError, e:
+        print "Didn't work, got error:",e
+    return test
 
-	return test
-
-def main():
-	request = Request('http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=rr_route_mfl')
-	#request = Request('http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=rr_route_bsl')
+def station_picker(station):
+    if station == 'mfl':
+        request = Request('http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=rr_route_mfl')
+    elif station == 'bsl':
+        request = Request('http://www3.septa.org/hackathon/Alerts/get_alert_data.php?req1=rr_route_bsl')
+    else:
+        print 'Please enter a valid station, either mfl or bsl'
     output = request_status(request)
-	
-	out = json.loads(output)
-	node = out[0]['advisory_message']
-
-	x = clean_html(node)
-	y = re.sub(r"\t+", "", x)
-	print y	
+    return output
+    
+def main():
+    for station in ['mfl', 'bsl']:
+        request_output = station_picker(station)
+        json_output = json_formatting(request_output)
+        cleaned_output = clean_html(json_output)
+        print cleaned_output
 
 if __name__ == "__main__":
 	main()
