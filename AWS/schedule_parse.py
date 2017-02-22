489a5a48534dd6441026970fcf7b72b3e7ad10fb
SKravitsky <drago509_6@hotmail.com>
2017-02-22 15:56:10 -0500

    Creating a csv for each line

--- AWS/schedule_parse.py
+++ AWS/schedule_parse.py
@@ -3,11 +3,28 @@
 import bs4 as bs
 import HTMLParser
 import csv
+import os.path
+import time
 from urllib2 import Request, urlopen, URLError
 
 dict = {}
 dict_list = {}
+lines = {
+    'mfl_69_w' : "http://www.septa.org/schedules/transit/w/MFL_1.htm",
+    'mfl_69_sat' : "http://www.septa.org/schedules/transit/s/MFL_1.htm",
+    'mfl_69_sun' : "http://www.septa.org/schedules/transit/h/MFL_1.htm",
+    'mfl_f_w' : "http://www.septa.org/schedules/transit/w/MFL_0.htm",
+    'mfl_f_sat' : "http://www.septa.org/schedules/transit/s/MFL_0.htm",
+    'mfl_f_sun' : "http://www.septa.org/schedules/transit/h/MFL_0.htm",
+    'bsl_fern_w' : "http://www.septa.org/schedules/transit/w/BSL_1.htm",
+    'bsl_fern_sat' : "http://www.septa.org/schedules/transit/s/BSL_1.htm",
+    'bsl_fern_sun' : "http://www.septa.org/schedules/transit/h/BSL_1.htm",
+    'bsl_att_w' : "http://www.septa.org/schedules/transit/w/BSL_0.htm",
+    'bsl_att_sat' : "http://www.septa.org/schedules/transit/s/BSL_0.htm",
+    'bsl_att_sun' : "http://www.septa.org/schedules/transit/h/BSL_0.htm",
+    }
 
+'''
 def schedule_Picker(line):
     return{
         'mfl_69_w' : "http://www.septa.org/schedules/transit/w/MFL_1.htm",
@@ -16,13 +33,21 @@ def schedule_Picker(line):
         'mfl_f_w' : "http://www.septa.org/schedules/transit/w/MFL_0.htm",
         'mfl_f_sat' : "http://www.septa.org/schedules/transit/s/MFL_0.htm",
         'mfl_f_sun' : "http://www.septa.org/schedules/transit/h/MFL_0.htm",
-	'bsl_fern_w' : "http://www.septa.org/schedules/transit/w/BSL_1.htm",
-	'bsl_fern_sat' : "http://www.septa.org/schedules/transit/s/BSL_1.htm",
-	'bsl_fern_sun' : "http://www.septa.org/schedules/transit/h/BSL_1.htm",
-	'bsl_att_w' : "http://www.septa.org/schedules/transit/w/BSL_0.htm",
-	'bsl_att_sat' : "http://www.septa.org/schedules/transit/s/BSL_0.htm",
-	'bsl_att_sun' : "http://www.septa.org/schedules/transit/h/BSL_0.htm",
-        }.get(line,'error')
+        'bsl_fern_w' : "http://www.septa.org/schedules/transit/w/BSL_1.htm",
+        'bsl_fern_sat' : "http://www.septa.org/schedules/transit/s/BSL_1.htm",
+        'bsl_fern_sun' : "http://www.septa.org/schedules/transit/h/BSL_1.htm",
+        'bsl_att_w' : "http://www.septa.org/schedules/transit/w/BSL_0.htm",
+        'bsl_att_sat' : "http://www.septa.org/schedules/transit/s/BSL_0.htm",
+        'bsl_att_sun' : "http://www.septa.org/schedules/transit/h/BSL_0.htm",
+    }.get(line,'error')
+'''
+
+def csv_Check(line_name, date):
+    csv_name = line_name + date
+    if os.path.isfile(csv_name):
+        return True
+    else:
+        return False
 
 
 def source(url_request):
@@ -52,11 +77,14 @@ def get_ID_Station(soup):
     for rows in table.findAll('th'):
         name = rows.text
         data = name.split("StopID:")
+        if data[0] == 'AT&T;':
+            data[0] = 'AT&T'
         temp.append(data[0])
         dict[data[1]] = data[0]
 
     list_half = temp[:len(temp)/2]
     return list_half
+
 
 ## Save the csv files with a time stamp
 ## have a separate script to upload the data
@@ -65,33 +93,42 @@ def csv_Writing(output):
         spamwriter = csv.writer(csvfile, delimiter = '|')
 
 
-def get_Time_Station(soup, list_half):
-    print(list_half)
+def get_Time_Station(soup, list_half, file_ending, key_name):
     for key, value in dict.iteritems():
         value2 = value.decode('utf-8')
         dict_list['%s' % value2] = []
     
     for key, value in dict.iteritems():
-        time = soup.find('td', title=value)
+        time = soup.find('td', title=value)        
         for tags in time.findAll('td'):
             #print(tags.string)
             temp = tags.string.replace(u'\xa0','')
             temp2 = temp.replace(u'\u2014', '-')
             dict_list['%s' % value].append(temp2)
     
-    with open('test.csv', 'wb') as outfile:
+    csv_file_name = key_name + file_ending
+    with open(csv_file_name, 'wb') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(dict_list.keys())
         writer.writerows(zip(*dict_list.values()))
     
         
-
 def main():
-    website = schedule_Picker('mfl_f_w')
-    main_source = source(website)
-    soup = soupify(main_source)
-    list_half = get_ID_Station(soup)
-    get_Time_Station(soup, list_half)
+    current_time = time.strftime("%m_%Y")
+    file_ending = "_" + current_time + ".csv"
+
+
+    for key, value in lines.iteritems():
+        if csv_Check(key, file_ending):
+            print(key + file_ending + " already exists")
+        else:
+            main_source = source(value)
+            soup = soupify(main_source)
+            list_half = get_ID_Station(soup)
+            get_Time_Station(soup, list_half, file_ending, key)
+            dict.clear()
+
+
 
 def lambda_handler(event, context):
     print event
