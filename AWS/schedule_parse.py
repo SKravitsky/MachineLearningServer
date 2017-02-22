50fef74249435dd41f06e32d68789ec2d8ef6ee5
SKravitsky <drago509_6@hotmail.com>
2017-02-21 19:58:32 -0500

    Working scheduler

--- AWS/schedule_parse.py
+++ AWS/schedule_parse.py
@@ -6,16 +6,25 @@ import csv
 from urllib2 import Request, urlopen, URLError
 
 dict = {}
+dict_list = {}
+
+def schedule_Picker(line):
+    return{
+        'mfl_69_w' : "http://www.septa.org/schedules/transit/w/MFL_1.htm",
+        'mfl_69_sat' : "http://www.septa.org/schedules/transit/s/MFL_1.htm",
+        'mfl_69_sun' : "http://www.septa.org/schedules/transit/h/MFL_1.htm",
+        'mfl_f_w' : "http://www.septa.org/schedules/transit/w/MFL_0.htm",
+        'mfl_f_sat' : "http://www.septa.org/schedules/transit/s/MFL_0.htm",
+        'mfl_f_sun' : "http://www.septa.org/schedules/transit/h/MFL_0.htm",
+        }.get(line,'error')
 
 
 def source(url_request):
-    site = Request(url_request)
-    
+    site = Request(url_request) 
     try:
         source = urlopen(site).read()
     except URLError, e:
         print "Didn't work, got error:", e
-
     return(source)
 
 
@@ -45,21 +54,30 @@ def csv_Writing(output):
 
 def get_Time_Station(soup):
     for key, value in dict.iteritems():
+        value2 = value.decode('utf-8')
+        dict_list['%s_list' % value2] = []
+    
+    for key, value in dict.iteritems():
         time = soup.find('td', title=value)
         for tags in time.findAll('td'):
-            print(tags.string)
+            #print(tags.string)
+            temp = tags.string.replace(u'\xa0','')
+            temp2 = temp.replace(u'\u2014', '-')
+            dict_list['%s_list' % value].append(temp2)
+    
+    with open('test.csv', 'wb') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(dict_list.keys())
+        writer.writerows(zip(*dict_list.values()))
+    
         
 
 def main():
-    website = 'http://www.septa.org/schedules/transit/w/MFL_1.htm'
+    website = schedule_Picker('mfl_f_w')
     main_source = source(website)
     soup = soupify(main_source)
     get_ID_Station(soup)
     get_Time_Station(soup)
-    for key,value in dict.iteritems():
-        print key, value
-        print "-----"
-
 
 def lambda_handler(event, context):
     print event
